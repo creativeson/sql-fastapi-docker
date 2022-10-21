52c6c7ba3e25c21fb842c279147bfa1340b5d4a8
Vincent <100126727+creativeson@users.noreply.github.com>
2022-10-21 12:28:31 +0800

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, TEXT
+from database import Base
+
+class Order(Base):
+    __tablename__ = "order"
+
+    order_id = Column(Integer, primary_key=True, index=True, nullable=True)
+    customer_name = Column(String)
+    customer_id = Column(String)
+    purchase_time = Column(TEXT)
+
+class Order_item(Base):
+    __tablename__ = "order item"
+
+    order_id = Column(Integer, primary_key=True, index=True, nullable=True)
+    product_name = Column(String)
+    amount = Column(Integer)
+    product_id = Column(String)
+    price = Column(Integer)
+
